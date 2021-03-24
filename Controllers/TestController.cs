c36d7db2140da91d34ae7b27508c749d9418d383
Andy Flynn <andy.flynn@aveva.com>
2021-03-24 14:57:25 +0000

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int? input)
+        {
+            _logger.LogInformation($"Input: {input}");
+            var result = new TransactionService().GetLetters(input);
+            _logger.LogInformation($"result: {result}");
+
+            return new JsonResult(result);
+        }
+    }
+}
